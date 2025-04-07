c94410ad5f471dc3fddc88a81ed31e52621096a3
Nitiksh <nitikshpal@gmail.com>
2025-04-07 15:40:15 +0530

    update the tailwind css issue

--- frontend/src/Pages/Home/home.jsx
+++ frontend/src/Pages/Home/home.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const Home = () => {
   return (
-    
-      <div>
-        Welcome to GYM Management 
-      </div>
-    
+    <div className="text-center text-2xl font-bold p-4 bg-green-100 rounded-lg shadow-md ">
+      Welcome to GYM Management
+    </div>
+
+
   )
 }
 
